2cf660cb4ebafd345e8b22649419d8bb56124338
Sameen Shah <sameenshah@berkeley.edu>
2021-07-20 17:06:39 -0400

    Working on business side interface. Added save methhod simple implementation.

--- client/src/note_access.ts
+++ client/src/note_access.ts
@@ -1,8 +1,10 @@
-import { Note } from './note_storage';
+import { Note, SimpleDataStore } from './note_storage';
 
 interface NoteAccess {
+    Validate(text: string): NoteAccessResult;
+    Validate(user: User): NoteAccessResult;
     Create(id: string): NoteAccessResult;
-    Update(note: Note): NoteAccessResult;
+    Save(note: Note): NoteAccessResult;
     Delete(id: string): NoteAccessResult;
     Get(id: string): NoteAccessResultWithNote<Note | null>;
 }
@@ -13,4 +15,51 @@ interface NoteAccessResult {
 
 interface NoteAccessResultWithNote<T> extends NoteAccessResult {
     GetResult(): T;
+}
+
+class inMemoryNoteAccessResult implements NoteAccessResult {
+    Success: boolean;
+
+    constructor(success: boolean) {
+        this.Success = success;
+    }
+
+    IsSuccess(): boolean {
+        return this.Success;
+    }
+}
+
+class SimpleNoteAccess implements NoteAccess {
+    Validate(text: string): NoteAccessResult {
+        if (text) {
+            return new inMemoryNoteAccessResult(true);
+        } else {
+            return new inMemoryNoteAccessResult(false);
+        }
+    }
+
+    Validate(user: User): NoteAccessResult {
+        if (user) {
+            return new inMemoryNoteAccessResult(true);
+        } else {
+            return new inMemoryNoteAccessResult(false);
+        }    
+    }
+
+    Save(note: Note): NoteAccessResult {
+        if (this.Validate(note.note).IsSuccess()) {
+            if (this.Validate(note.author).IsSuccess()) {
+                SimpleDataStore.Save(note);
+                // replace these print statements with UI method calls
+                console.log('Successfully Saved Note');
+                return new inMemoryNoteAccessResult(true);
+            } else {
+                console.log('Invalid User/Create Account');
+                return new inMemoryNoteAccessResult(false);
+            }
+        } else {
+            console.log('Invalid Note');
+            return new inMemoryNoteAccessResult(false);
+        }
+    }
 }
