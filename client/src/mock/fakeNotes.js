528c9e3eb3d0f9eeef34b3a6e4062fd5381f121c
shipitsteven <stevenwangbe@gmail.com>
2021-07-29 15:11:30 -0700

    added mock tags accordion

--- client/src/mock/fakeNotes.js
+++ client/src/mock/fakeNotes.js
@@ -20,3 +20,15 @@ export const fakeNotes = [
     date: '2015-03-12',
   },
 ]
+
+export const fakeTags = [
+  { name: 'school', color: '##FFAEBC' },
+  { name: 'work', color: '#FFD1A5' },
+  { name: 'home', color: '#FFF8E1' },
+  { name: 'friends', color: '#90F1EF' },
+  { name: 'basketball', color: '#FFA384' },
+  { name: 'soccer', color: '#7BF1A8' },
+  { name: 'travel', color: '#854D27' },
+  { name: 'music', color: '#475B63' },
+  { name: 'movies', color: '#f72585' },
+]
